dd59f09f68b52902e25ef232f3377c8cb1e9c580
RCDCHR92R50B202X <chiara_arcidiacono@yahoo.it>
2019-12-09 12:45:36 +0100

    first commit

---
+++ hello3/src/main/java/on/hello3/App.java
@@ -0,0 +1,13 @@
+package on.hello3;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
